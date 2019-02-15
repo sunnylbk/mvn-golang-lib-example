b231d222b573182f96b1b7045db7fd51f6e5088c
Bharath Kumar <bharath.kumar@adaptive.team>
2019-02-15 12:46:02 +0530

    Added lib and lib test code

---
+++ mvn-golang-lib/src/com/sunnylbk/mvngotest/SomeUtils.go
@@ -0,0 +1,10 @@
+package mvngotest
+
+import "fmt"
+
+var Buildstamp string
+var svnRevision string
+
+func SomeTestMethod() {
+	fmt.Printf("Hello from mvn-go-test-lib module!\n")
+}
---
+++ mvn-golang-test-lib/src/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "com/sunnylbk/mvngotest"
+
+func main() {
+	mvngotest.SomeTestMethod()
+}
